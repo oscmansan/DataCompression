2be25442917b48891f44f4be6d9f0b520e05a027
Oscar Mañas <oscmansan@gmail.com>
2017-03-14 19:11:55 +0100

    Implement Shannon code

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3.6
 from collections import defaultdict
 from itertools import product
-from math import log
+from math import log, ceil
+from string import ascii_lowercase
+from pprint import pprint, pformat
 
 
 def to_probabilities(src):
     letters, count = zip(*src)
-    probs = list(map(lambda x: float(x) / sum(count), count))
+    probs = list(map(lambda x: float(x)/sum(count), count))
     return zip(letters, probs)
 
 
@@ -43,9 +45,36 @@ def entropy_source(src):
     return H
 
 
-with open('../data/don_quijote.txt') as f:
+def shannon_code(src):
+    src = to_probabilities(src)
+    src = sorted(src, key=lambda x: x[1], reverse=True)
+
+    letters, probs = map(list,zip(*src))
+    L = list(map(lambda x: ceil(log(1/x,2)),probs)) # shannon code
+
+    C = []
+    q = ['']
+    for n in range(max(L)+1):
+        while L and L[0]==n:
+            L.pop(0)
+            x = q.pop(0)
+            a = letters.pop(0)
+            C.append((a,x))
+        else:
+            aux = []
+            for x in q:
+                aux.append(x+'0')
+                aux.append(x+'1')
+            q = aux
+
+    return C
+
+
+with open('../data/moby_dick.txt') as f:
     txt = f.read()
+    txt = filter(lambda c: c in ascii_lowercase+' ',txt.lower())
     src = source(txt)
-    print(src)
-    print(source_extension(src, 2))
-    print(entropy_source(src))
+    #pprint(sorted(src,key=lambda x:x[1],reverse=True))
+    #pprint(sorted(source_extension(src, 2),key=lambda x:x[1],reverse=True))
+    print('H = ' + str(entropy_source(src)))
+    print('C = ' + pformat(shannon_code(src)))
