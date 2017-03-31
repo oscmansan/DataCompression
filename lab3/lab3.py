a05e72f4d0c6dbbd5cadfd76c31e4388abc034ff
Oscar Mañas <oscmansan@gmail.com>
2017-04-01 00:58:37 +0200

    WIP

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3.6
 from collections import defaultdict
-from string import ascii_lowercase
-from math import log, ceil
 
 
 def source(str):
@@ -19,15 +17,12 @@ def cumsum(L):
 
 
 def binary(x, k):
-    if x == 0:
-        return '0' * k
-    else:
-        y = bin(x)[2:]
-        return '0' * (k - len(y)) + y
+    y = bin(x)[2:]
+    return '0' * (k - len(y)) + y
 
 
 def arithmetic_encode(str, src, k):
-    n = sum(map(lambda x: x[1], src))
+    l = sum(map(lambda x: x[1], src))
     letters = {}
     for i, a in enumerate(map(lambda x: x[0], src)):
         letters[a] = i
@@ -43,11 +38,12 @@ def arithmetic_encode(str, src, k):
     for x in str:
         # update to the next subinterval
         i = letters[x]
-        _a = a + d * cumcount[i] // n
-        _b = a + d * cumcount[i + 1] // n - 1
+        _a = a + d * cumcount[i] // l
+        _b = a + d * cumcount[i + 1] // l - 1
 
         _a = binary(_a, k)
         _b = binary(_b, k)
+        print(x, _a, _b)
 
         # rescaling
         while _a[0] == _b[0]:
@@ -72,7 +68,61 @@ def arithmetic_encode(str, src, k):
     return c + '1'
 
 
+def arithmetic_decode(c,src,k,L):
+    n = len(src)
+    count = list(map(lambda x: x[1], src))
+    cumcount = cumsum(count)
+    l = sum(count)
+    letters = list(map(lambda x: x[0], src))
+
+    a = 0
+    b = 2 ** k - 1
+    d = b - a + 1
+
+    g = c[:k]
+    c = c[k:]
+
+    str = ''
+    for _ in range(L):
+        _g = int(g, 2)
+
+        # find next letter
+        for i in range(n):
+            _a = a + d * cumcount[i] // l
+            _b = a + d * cumcount[i + 1] // l - 1
+            if _a <= _g <= _b:
+                str += letters[i]
+                break
+
+        _a = binary(_a, k)
+        _b = binary(_b, k)
+        print(_a, _b, g, str)
+
+        # rescaling
+        while _a[0] == _b[0]:
+            _a = _a[1:] + '0'
+            _b = _b[1:] + '1'
+            g = g[1:] + c[0]
+            c = c[1:] + '0'
+
+        # underflow prevent
+        while _a[1] == '1' and _b[1] == '0':
+            _a = '0' + _a[2:] + '0'
+            _b = '1' + _b[2:] + '1'
+            g = g[0] + g[2:] + c[0]
+            c = c[1:] + '0'
+
+        a = int(_a, 2)
+        b = int(_b, 2)
+        d = b - a + 1
+
+    return str
+
+
 txt = '1010000000'
 src = [('0', 9), ('1', 1)]
-c = arithmetic_encode(txt, src, 6)
+k = 6
+c = arithmetic_encode(txt, src, k)
 print(c)
+txt = arithmetic_decode(c,src,k,len(txt))
+print(txt)
